f69243369775f61f01a1426bf6c63d1e2f57d14f
Murdo R Ergeaux <visual.melon@gmail.com>
2017-09-19 15:59:59 +0100

    Add Everything

---
+++ OxyHisto/ContinuousHistogramItem.cs
@@ -0,0 +1,178 @@
+﻿
+namespace OxyPlot.Series
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// Represents an item in a <see cref="ContinuousHistogramSeries" />, a bin (range) and its area.
+    /// </summary>
+    public class ContinuousHistogramItem
+    {
+        /// <summary>
+        /// A range in a histogram, and it's area
+        /// </summary>
+        /// <param name="rangeStart">The range start.</param>
+        /// <param name="rangeEnd">The range end.</param>
+        /// <param name="area">The area.</param>
+        /// <param name="color">The color.</param>
+        public ContinuousHistogramItem(double rangeStart, double rangeEnd, double area, OxyColor color)
+        {
+            RangeStart = rangeStart;
+            RangeEnd = rangeEnd;
+            Area = area;
+            this.Color = color;
+        }
+        
+        /// <summary>
+        /// A range in a histogram, and it's area
+        /// </summary>
+        /// <param name="rangeStart">The range start.</param>
+        /// <param name="rangeEnd">The range end.</param>
+        /// <param name="area">The area.</param>
+        public ContinuousHistogramItem(double rangeStart, double rangeEnd, double area)
+        {
+            RangeStart = rangeStart;
+            RangeEnd = rangeEnd;
+            Area = area;
+
+            this.Color = OxyColors.Automatic;
+        }
+
+        /// <summary>
+        /// Gets or sets the color of the item.
+        /// </summary>
+        /// <remarks>If the color is not specified (default), the color of the series will be used.</remarks>
+        public OxyColor Color { get; set; }
+
+        /// <summary>
+        /// Gets or sets the range start.
+        /// </summary>
+        /// <value>The range start.</value>
+        public double RangeStart { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the range end.
+        /// </summary>
+        /// <value>The range end.</value>
+        public double RangeEnd { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the area.
+        /// </summary>
+        /// <value>The area.</value>
+        public double Area { get; set; }
+
+        /// <summary>
+        /// Gest the computed width of the item.
+        /// </summary>
+        public double Width => this.RangeEnd - this.RangeStart;
+        
+        /// <summary>
+        /// Gest the computed height of the item.
+        /// </summary>
+        public double Height => this.Area / this.Width;
+        
+        /// <summary>
+        /// Gets the value of the item. Equivalent to the Height;
+        /// </summary>
+        /// <value>The value can be used to color-code the rectangle.</value>
+        public double Value => Height;
+
+        /// <summary>
+        /// Determines whether the specified point lies within the boundary of the rectangle.
+        /// </summary>
+        /// <returns><c>true</c> if the value of the <param name="p"/> parameter is inside the bounds of this instance.</returns>
+        public bool Contains(DataPoint p)
+        {
+            return (p.X <= this.RangeEnd && p.X >= this.RangeStart && p.Y <= this.Height && p.Y >= 0) ||
+                   (p.X <= this.RangeStart && p.X >= this.RangeEnd && p.Y <= this.Height && p.Y >= 0);
+        }
+        
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this instance.
+        /// </summary>
+        /// <returns>A <see cref="System.String" /> that represents this instance.</returns>
+        public override string ToString()
+        {
+            return string.Format(
+                "{0} {1} {2}",
+                this.RangeStart,
+                this.RangeEnd,
+                this.Area);
+        }
+    }
+
+    public static class HistogramHelpers
+    {
+        public static IEnumerable<ContinuousHistogramItem> Collect(IEnumerable<double> samples, double start, double end, int binCount, bool countUnplaced)
+        {
+            List<double> binBreaks = new List<double>(binCount);
+
+            for (int i = 0; i <= binCount; i++)
+            {
+                binBreaks.Add(start + ((end - start) / binCount) * i);
+            }
+
+            return Collect(samples, binBreaks, countUnplaced);
+        }
+
+        public static IEnumerable<ContinuousHistogramItem> Collect(IEnumerable<double> samples, IReadOnlyList<double> binBreaks, bool countUnplaced)
+        {
+            // determin ranges
+            double[] orderedBreaks = binBreaks.Distinct().OrderBy(b => b).ToArray(); // TODO: resolve distinct
+
+            // count samples
+            List<int> counts = new List<int>();
+            long total = 0;
+            
+            for (int i = 0; i < binBreaks.Count - 1; i++)
+            {
+                counts.Add(0);
+            }
+
+            foreach (double sample in samples)
+            {
+                int idx = System.Array.BinarySearch(orderedBreaks, sample);
+
+                bool placed = false;
+                
+                if (idx >= 0)
+                {
+                    // exact match, place in the corresponding bin (exclude last bin)
+                    if (idx < counts.Count)
+                    {
+                        counts[idx] += 1;
+                        placed = true;
+                    }
+                }
+                else
+                {
+                    // inexact match, place in lower bin
+                    idx = ~idx - 1;
+                    
+                    if (idx >= 0 && idx < counts.Count)
+                    {
+                        counts[idx] += 1;
+                        placed = true;
+                    }
+                }
+
+                if (placed || countUnplaced)
+                {
+                    total++;
+                }
+            }
+
+            // create items
+            List<ContinuousHistogramItem> items = new List<ContinuousHistogramItem>(counts.Count);
+            
+            for (int i = 0; i < binBreaks.Count - 1; i++)
+            {
+                items.Add(new ContinuousHistogramItem(binBreaks[i], binBreaks[i + 1], (double)counts[i] / total));
+            }
+
+            return items;
+        }
+    }
+}
---
+++ OxyHisto/Form1.cs
@@ -0,0 +1,66 @@
+﻿using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OxyHisto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs eargs)
+        {
+            var model = new PlotModel { Title = "Continuous Histograms", Subtitle = "Distribution of cos(x) values" };
+            model.Axes.Add(new LinearAxis { Position = AxisPosition.Left, MajorGridlineStyle = LineStyle.Solid, Key = "YBottom", StartPosition = 0, EndPosition = 0.48, AbsoluteMinimum = 0, AbsoluteMaximum = 5, Maximum = 5 });
+            model.Axes.Add(new LinearAxis { Position = AxisPosition.Left, MajorGridlineStyle = LineStyle.Solid, Key = "YTop", StartPosition = 0.52, EndPosition = 1.0, AbsoluteMinimum = 0, AbsoluteMaximum = 5, Maximum = 5 });
+            model.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom });
+
+            var chs1 = new ContinuousHistogramSeries() { YAxisKey = "YBottom" } ;
+            chs1.ItemsSource = HistogramHelpers.Collect(RandomSource(10000), 0, 1, 10, true);
+            chs1.StrokeThickness = 1;
+            model.Series.Add(chs1);
+
+            var chs2 = new ContinuousHistogramSeries() { YAxisKey = "YTop" } ;
+            chs2.ItemsSource = HistogramHelpers.Collect(RandomSource(10000), new[] { 0, 0.02, 0.05, 0.1, 0.2, 0.4, 0.6, 0.8, 0.9, 0.95, 0.98, 1.0 }, true);
+            chs2.StrokeThickness = 1;
+            model.Series.Add(chs2);
+
+            OxyPlot.WindowsForms.PlotView plotView = new OxyPlot.WindowsForms.PlotView();
+
+            plotView.Model = model;
+
+            this.Controls.Add(plotView);
+
+            model.InvalidatePlot(true);
+            plotView.Invalidate();
+
+            plotView.Dock = DockStyle.Fill;
+
+            this.Refresh();
+        }
+
+        private static IEnumerable<double> RandomSource(int count)
+        {
+            Random rnd = new Random();
+
+            for (int i = 0; i < count; i++)
+            {
+                double r = rnd.NextDouble();
+                yield return Math.Cos(r * Math.PI * 2.0) * 0.5 + 0.5;
+            }
+        }
+    }
+}
+
